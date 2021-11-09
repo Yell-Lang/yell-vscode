104afb3c49af4ba56dfda0cafcfda241dc64db4a
Rudra Saraswat <rs2009@ubuntu.com>
2021-11-09 16:26:14 +0530

    Optimize extension

--- out/extension.js
+++ out/extension.js
@@ -11,12 +11,13 @@ class YellCompletionItemProvider {
         if (linePrefix.endsWith('print ') || linePrefix.endsWith('println ') || linePrefix.endsWith('sleep ')
             || linePrefix.endsWith('system ')) {
             return [
+                new vscode.CompletionItem("'';", vscode.CompletionItemKind.Property),
                 new vscode.CompletionItem('"";', vscode.CompletionItemKind.Property),
             ];
         }
         else if (linePrefix.endsWith('read ')) {
             return [
-                new vscode.CompletionItem('"input" "input_var";', vscode.CompletionItemKind.Property),
+                new vscode.CompletionItem('"input: " "input_var";', vscode.CompletionItemKind.Property),
             ];
         }
         return [
@@ -25,7 +26,13 @@ class YellCompletionItemProvider {
             new vscode.CompletionItem('var', vscode.CompletionItemKind.Function),
             new vscode.CompletionItem('read', vscode.CompletionItemKind.Function),
             new vscode.CompletionItem('sleep', vscode.CompletionItemKind.Function),
+            new vscode.CompletionItem('a', vscode.CompletionItemKind.Function),
+            new vscode.CompletionItem('import', vscode.CompletionItemKind.Function),
             new vscode.CompletionItem('system', vscode.CompletionItemKind.Function),
+            new vscode.CompletionItem('python', vscode.CompletionItemKind.Function),
+            new vscode.CompletionItem('while', vscode.CompletionItemKind.Function),
+            new vscode.CompletionItem('if', vscode.CompletionItemKind.Function),
+            new vscode.CompletionItem('repeat', vscode.CompletionItemKind.Function),
             new vscode.CompletionItem('println', vscode.CompletionItemKind.Function),
             new vscode.CompletionItem('println "Hello World!";', vscode.CompletionItemKind.Snippet),
         ];
@@ -83,6 +90,8 @@ function updateDiagnostics(editor) {
                 case 'repeat':
                     break;
                 case 'read':
+                    break;
+                case 'python':
                     break;
                 case 'a':
                     break;
